98250a3572e10cad9b019fa5f1a24833861408aa
genesissiana <perezgenesisa@gmail.com>
2021-09-18 16:16:34 -0700

    using react router, server broke

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,21 @@
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import React from 'react';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
-
 import Login from './pages/Login';
 import NoMatch from './pages/NoMatch';
 import SingleThought from './pages/SingleThought';
 import Profile from './pages/Profile';
 import Signup from './pages/Signup';
 
+// To establish the connection to the back-end server's /graphql endpoint
 const httpLink = createHttpLink({
+  // URI stands for "Uniform Resource Identifier."
   uri: '/graphql',
 });
 
@@ -29,20 +31,21 @@ function App() {
         <div className="flex-column justify-flex-start min-100-vh">
           <Header />
           <div className="container">
-  <Switch>
-    <Route exact path="/" component={Home} />
-    <Route exact path="/login" component={Login} />
-    <Route exact path="/signup" component={Signup} />
-    <Route exact path="/profile/:username?" component={Profile} />
-    <Route exact path="/thought/:id" component={SingleThought} />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/profile/:username?" component={Profile} />
+              <Route exact path="/thought/:id" component={SingleThought} />
 
-    <Route component={NoMatch} />
-  </Switch>
-</div>
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
           <Footer />
         </div>
       </Router>
     </ApolloProvider>
   );
 }
+
 export default App;
--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  // context: authMiddleware
+  context: authMiddleware
 });
 
 // integrate our Apollo server with the Express application as middleware
